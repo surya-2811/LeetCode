878762957d999b53f435c391ebd3ed3a6a58c171
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-05-19 13:22:24 +0530

    Update 15. 3Sum.js

--- 15. 3Sum.js
+++ 15. 3Sum.js
@@ -8,30 +8,39 @@
 
 var threeSum = function(nums) {
     let res = [];
-    nums = nums.sort((a, b)=>a-b)
-    for(let i=0; i <nums.length -2; i++){
-        if(i > 0 && nums[i] === nums[i-1]) continue
-        let j = i+1;
-        let k = nums.length -1;
-        while(j<k){
-            let sum = nums[i] + nums[j] + nums[k]
-            if(sum === 0){
-                res.push([nums[i], nums[j], nums[k]]);
-                while(j< k && nums[j] === nums[j-1]) j++  
-                while(j< k && nums[k] === nums[k+1]) k--  
+    nums.sort((a, b) => a - b);  // Sort the array
 
-                j++
-                k--
-            }else if(sum < 0){
-                j++
-            }else{
-                k--
+    for (let i = 0; i < nums.length - 2; i++) {
+        // Skip duplicate elements for the first number
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let j = i + 1;
+        let k = nums.length - 1;
+
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+
+            if (sum === 0) {
+                res.push([nums[i], nums[j], nums[k]]);
+                
+                // Skip duplicates for the second number
+                while (j < k && nums[j] === nums[j + 1]) j++;
+                // Skip duplicates for the third number
+                while (j < k && nums[k] === nums[k - 1]) k--;
+
+                j++;
+                k--;
+            } else if (sum < 0) {
+                j++;
+            } else {
+                k--;
             }
         }
     }
-    return res
+
+    return res;
 };
 
-let nums = [-1,0,1,2,-1,-4]
-
-console.log(threeSum(nums))
+// Example usage
+const nums = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(nums));  // Output: [[-1, -1, 2], [-1, 0, 1]]
